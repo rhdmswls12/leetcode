b806b863e4f933b6c55806bec87e3df59c62f10a
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-11-02 21:00:09 +0900

    Time: 1867 ms, Memory: 41.2 MB - LeetHub

---
+++ target-sum/target-sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var findTargetSumWays = function(nums, target) {
+    let count = 0
+    
+    function dfs(idx, sum, target) {
+        if (idx === nums.length) {
+            if (sum === target) {
+                count++
+            }
+            return
+        }
+        dfs(idx+1, sum + nums[idx], target)
+        dfs(idx+1, sum - nums[idx], target)
+    }
+    dfs(0, 0, target)
+    return count
+};

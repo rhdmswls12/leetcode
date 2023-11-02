1ff3ce36b5f1266af274ce98a508d07651bcd586
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-11-02 19:43:01 +0900

    Time: 58 ms, Memory: 45.3 MB - LeetHub

---
+++ path-sum/path-sum.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+
+var hasPathSum = function(root, targetSum) {
+    
+    const dfs = (node, sum, targetSum) => {
+    if (!node) {
+        return false
+    }
+        
+    sum += node.val
+        
+    if (node.left === null && node.right === null) {
+        return sum === targetSum
+    }
+    return dfs(node.left, sum, targetSum) || dfs(node.right, sum, targetSum)
+ }
+    
+    return dfs(root, 0, targetSum)
+};

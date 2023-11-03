d0a35f33ff8ab626de6c7a3dd0e577f4afef2566
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-11-03 20:28:51 +0900

    Time: 61 ms, Memory: 44.6 MB - LeetHub

--- number-of-islands/number-of-islands.js
+++ number-of-islands/number-of-islands.js
@@ -3,27 +3,26 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    const dfs = (x, y) => {
-        if (x <= -1 || x >= grid.length || y <= -1 || y >= grid[0].length) {
+    let count = 0
+    
+    function dfs(x, y) {
+        if (x<0 || x>=grid.length || y<0 || y>=grid[0].length) {
             return false
         }
-        if(grid[x][y] == 1) {
-            grid[x][y] = 0
-            dfs(x-1, y)
-            dfs(x, y-1)
+        if (grid[x][y] === '1') {
+            grid[x][y] = '0'
             dfs(x+1, y)
             dfs(x, y+1)
+            dfs(x-1, y)
+            dfs(x, y-1)
             return true
         }
         return false
     }
-    let answer = 0
     for (let i=0; i<grid.length; i++) {
         for (let j=0; j<grid[0].length; j++) {
-            if (dfs(i, j)) {
-                answer++
-            }
+            if (dfs(i,j)) count++
         }
     }
-    return answer
+    return count
 };

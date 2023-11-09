062134f2eb4027b6f1a762d0d4fedbfd14609fbf
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-11-09 12:22:37 +0900

    Time: 62 ms, Memory: 45.7 MB - LeetHub

--- evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
+++ evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -6,17 +6,16 @@ var evalRPN = function(tokens) {
     let stack = []
     
     for (let i=0; i<tokens.length; i++) {
-        if(Number(tokens[i]) || Number(tokens[i] == 0)) {
-            stack.push(Number(tokens[i]))
-            console.log(stack)
-        } else {
-            let result = calculate(stack.pop(), stack.pop(), tokens[i])
+        if (!Number(tokens[i]) && Number(tokens[i]) !== 0) {
+            let result = calculate(Number(stack.pop()), Number(stack.pop()), tokens[i])
             stack.push(result)
-            console.log(stack)
+        } else {
+            stack.push(tokens[i])
         }
     }
-    return stack.pop()
+    if (stack.length === 1) return stack[0]
 };
+
 const calculate = (num1, num2, operator) => {
     if (operator === '+') return num2 + num1
     if (operator === '-') return num2 - num1

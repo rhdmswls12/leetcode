14e77183cc6dc4e52a0b736962bc3c09c004924e
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-12-13 20:09:21 +0900

    Time: 70 ms (66.88%), Space: 44.8 MB (76.96%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -0,0 +1,27 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    
+    function dfs(x, y) {
+        if (x<0 || x>=grid.length || y<0 || y>=grid[0].length) return false
+        
+        if (grid[x][y] === '1') {
+            grid[x][y] = '0'
+            dfs(x+1, y)
+            dfs(x, y+1)
+            dfs(x-1, y)
+            dfs(x, y-1)
+            return true
+        }
+        return false
+    }
+    let answer = 0
+    for (let i=0; i<grid.length; i++) {
+        for (let j=0; j<grid[0].length; j++) {
+            if (dfs(i, j)) answer++
+        }
+    }
+    return answer
+};
